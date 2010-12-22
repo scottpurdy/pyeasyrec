d31ffa0d4fd0c844c571a0183be8e7b08b575d3b
Pete Hunt <floydophone@gmail.com>
2010-12-22 11:01:39 -0500

    Added setup script.

---
+++ setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+
+setup(name="PyEasyrec",
+      version="1.0",
+      description="Python bindings for easyrec.",
+      author="Scott Purdy",
+      py_modules = ["easyrec"],)
